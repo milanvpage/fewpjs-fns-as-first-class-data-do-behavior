27d3ce3694074492e51d4da08a7b7c262241c1e3
Milan <milanvpage@gmail.com>
2021-09-08 07:05:48 -0700

    commit tree 95ff023c7bef79b4992d02bc573360dfad2557cc

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = time.split(':');
+  const parsed = parseInt(hour[0], 10);
+  if (parsed < 12) {
+    return "Good Morning"
+  } else if (parsed > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+
+
+function displayMessage(message) {
+  const content = document.getElementById('greeting')
+  content.textContent = message
+} 
